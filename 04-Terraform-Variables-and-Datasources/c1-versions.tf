15f24a11fe148a2d815552bb81d32b1becace985
guptarahul34 <guptarahul34886@gmail.com>
2025-04-04 09:59:21 +0000

    add module 04

---
+++ 04-Terraform-Variables-and-Datasources/c1-versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.11.2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.93.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = ""
+  secret_key = ""
+}
+
